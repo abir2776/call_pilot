ba58d265f65ad775f50c457ae2f4babb6bd8094a
osmangoni255 <osmangoni255@gmail.com>
2026-02-15 16:02:54 +0600

    fixed urls for status

--- interview/rest/urls/__init__.py
+++ interview/rest/urls/__init__.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("", include("interview.rest.urls.interview")),
     path("conversations/", include("interview.rest.urls.conversations")),
     path("call/config/", include("interview.rest.urls.call_config")),
+    path("status/", include("interview.rest.urls.status")),
 ]
