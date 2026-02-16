cd3611946a923e6a0624dd1544d72f22c6a093a2
abir2776 <osmangoni255@gmail.com>
2026-02-16 16:48:24 +0600

    fixed url issue

--- organizations/rest/views/register.py
+++ organizations/rest/views/register.py
@@ -71,7 +71,7 @@ class UserForgetPasswordAPIView(APIView):
         user.save()
         context = {
             "username": user.get_full_name(),
-            "verification_link": f"{settings.FRONTEND_BASE_URL}/resetPassword/{user.token}?email={email}",
+            "verification_link": f"{settings.FRONTEND_BASE_URL}/forgot-password-verify/{user.token}?email={email}",
             "current_year": 2025,
         }
         send_email_task.delay(
